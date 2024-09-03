5564d6537ca72ddbeca7ee82b58cda9a6d6cb52d
non-negotiable forge <mnvthisside@gmail.com>
2024-09-03 15:48:43 +0530

    update index.js, made express api in app.js & add utils files

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on PORT: ${process.env.PORT}`)
+  })
+})
+.catch(() => {
+    console.log("MongoDB Connection Failed !!",  err)
+})
